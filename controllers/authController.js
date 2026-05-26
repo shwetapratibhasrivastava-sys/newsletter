f11fea1864715e04ce1b6755a4c08673d620b336
Shweta Srivastava <shwetapratibhasrivastava@gmail.com>
2026-05-26 16:40:49 +0530

    authcreated

--- controllers/authController.js
+++ controllers/authController.js
@@ -37,3 +37,40 @@ export const register = async (req, res) => {
     });
   }
 };
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({
+        message: "Email and password are required",
+      });
+    }
+
+    const user = await Auth.findOne({ email });
+
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found",
+      });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(401).json({
+        message: "Invalid password",
+      });
+    }
+
+    res.status(200).json({
+      message: "Login successful",
+      data: user,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,11 +1,12 @@
 import express from "express"
-import { register } from "../controllers/authController.js"
+import { login, register } from "../controllers/authController.js"
 
 
 const authRoute=express.Router()
 
 
 authRoute.post("/register",register)
+authRoute.post("/login",login)
 
 
 
